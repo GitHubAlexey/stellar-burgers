631af9fcc9e982ebd8f3f4fd85384cf460d2f353
Alexey Salyukov <alexey.salyukov@yandex.ru>
2024-09-20 08:19:28 +0300

    Project 12_Review 02

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,6 +1,15 @@
 import cypress from 'cypress';
 
 const url = 'https://norma.nomoreparties.space/api';
+const burgerConstructor = '[data-cy="burgerConstructor"]';
+const main = '[data-cy="main"]';
+const bun = '[data-cy="bun"]';
+const sauce = '[data-cy="sauce"]';
+const orderButton = '[data-cy="orderButton"]';
+const orderNumber = '[data-cy="orderNumber"]';
+const modal = '[data-cy="modal"]';
+const modalCloseButton = '[data-cy="modalCloseButton"]';
+const modalOverlay = '[data-cy="modalOverlay"]';
 
 beforeEach(() => {
   cy.intercept('GET', `${url}/ingredients`, {
@@ -10,7 +19,7 @@ beforeEach(() => {
   localStorage.setItem('refreshToken', 'REFRESH_TOKEN');
   cy.intercept('GET', `${url}/auth/user`, { fixture: 'login.json' });
   cy.intercept('POST', `${url}/orders`, { fixture: 'order.json' });
-  cy.visit('http://localhost:4000');
+  cy.visit('/');
 });
 
 afterEach(() => {
@@ -20,73 +29,67 @@ afterEach(() => {
 
 describe('добавление ингредиента из списка в конструктор', () => {
   it('добавление одного ингредиента и соуса из списка в конструктор', () => {
-    cy.get('[data-cy="main"]').eq(1).children('button').click();
-    cy.get('[data-cy="burgerConstructor"]').should(
+    cy.get(main).eq(1).children('button').click();
+    cy.get(burgerConstructor).should(
       'contain',
       'Филе Люминесцентного тетраодонтимформа'
     );
-    cy.get('[data-cy="sauce"]').eq(2).children('button').click();
-    cy.get('[data-cy="burgerConstructor"]').should(
+    cy.get(sauce).eq(2).children('button').click();
+    cy.get(burgerConstructor).should(
       'contain',
       'Соус традиционный галактический'
     );
   });
 
   it('добавление булки', () => {
-    cy.get('[data-cy="bun"]').eq(0).children('button').click();
-    cy.get('[data-cy="burgerConstructor"]').should(
-      'contain',
-      'Краторная булка N-200i'
-    );
+    cy.get(bun).eq(0).children('button').click();
+    cy.get(burgerConstructor).should('contain', 'Краторная булка N-200i');
   });
 });
 
 describe('работа модальных окон', () => {
   beforeEach(() => {
-    cy.get('[data-cy="bun"]').eq(0).click();
+    cy.get(bun).eq(0).click();
   });
 
   it('открытие модального окна ингредиента', () => {
-    cy.get('[data-cy="modal"]').should('be.visible');
+    cy.get(modal).should('be.visible');
     cy.reload(true);
-    cy.get('[data-cy="modal"]').should('be.visible');
+    cy.get(modal).should('be.visible');
   });
 
   it('закрытие по клику на крестик', () => {
-    cy.get('[data-cy="modalCloseButton"]').click();
-    cy.get('[data-cy="modal"]').should('not.exist');
+    cy.get(modalCloseButton).click();
+    cy.get(modal).should('not.exist');
   });
 
   it('закрытие по клику на оверлей', () => {
-    cy.get('[data-cy="modalOverlay"]').click({ force: true });
-    cy.get('[data-cy="modal"]').should('not.exist');
+    cy.get(modalOverlay).click({ force: true });
+    cy.get(modal).should('not.exist');
   });
 });
 
 describe('создание заказа', () => {
   it('создание заказа', () => {
     // вызывается клик по кнопке «Оформить заказ»
-    cy.get('[data-cy="orderButton"]').should('be.disabled');
-    cy.get('[data-cy="bun"]').eq(0).children('button').click();
-    cy.get('[data-cy="main"]').eq(1).children('button').click();
-    cy.get('[data-cy="sauce"]').eq(2).children('button').click();
-    cy.get('[data-cy="orderButton"]').should('be.enabled');
-    cy.get('[data-cy="orderButton"]').click();
+    cy.get(orderButton).should('be.disabled');
+    cy.get(bun).eq(0).children('button').click();
+    cy.get(main).eq(1).children('button').click();
+    cy.get(sauce).eq(2).children('button').click();
+    cy.get(orderButton).should('be.enabled');
+    cy.get(orderButton).click();
 
     // проверяется, что модальное окно открылось и номер заказа верный
-    cy.get('[data-cy="modal"]').should('be.visible');
-    cy.get('[data-cy="orderNumber"]').should('contain.text', '53401');
+    cy.get(modal).should('be.visible');
+    cy.get(orderNumber).should('contain.text', '53401');
 
     // закрывается модальное окно и проверяется успешность закрытия
-    cy.get('[data-cy="modalCloseButton"]').click();
-    cy.get('[data-cy="modal"]').should('not.exist');
+    cy.get(modalCloseButton).click();
+    cy.get(modal).should('not.exist');
 
     // проверяется, что конструктор пуст
-    cy.get('[data-cy="orderButton"]').should('be.disabled');
-    cy.get('[data-cy="burgerConstructor"]').should(
-      'contain',
-      'Выберите начинку'
-    );
-    cy.get('[data-cy="burgerConstructor"]').should('contain', 'Выберите булки');
+    cy.get(orderButton).should('be.disabled');
+    cy.get(burgerConstructor).should('contain', 'Выберите начинку');
+    cy.get(burgerConstructor).should('contain', 'Выберите булки');
   });
 });
